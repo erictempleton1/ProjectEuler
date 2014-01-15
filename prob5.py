c8ec3b59567c661f8ac61f4bdb8cdf6661b29be7
unknown <eric.templeton@DWUS01LT01770.totalent>
2014-01-15 14:36:52 -0500

    first attempt

--- prob5.py
+++ prob5.py
@@ -3,4 +3,10 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
+mult_list = []
+for x in range(1,11):
+    for y in range(1,11):
+        mult_list.append(x*y)
 
+print mult_list
+        
