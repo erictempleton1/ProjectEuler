e560caca8eb448714199217257f565d1624df2d0
Eric Templeton <erictempleton1@gmail.com>
2015-04-05 21:33:35 -0400

    actual solution to prob 1

--- JavaScript/prob1.js
+++ JavaScript/prob1.js
@@ -1,4 +1,8 @@
-for (var num = 0; num < 10; num += 1) {
+// find the sum of all multiples of 3 or 5 below 1000
+
+var num_arr = []
+var totalled = 0
+for (var num = 0; num < 1000; num += 1) {
     if (num % 3 == 0 || num % 5 == 0)
-        console.log(num);
+        totalled += num;
 };
