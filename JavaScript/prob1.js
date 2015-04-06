9b8d0cfc7e1d6514ad79848aa10a1ef2b9f4eb5b
Eric Templeton <erictempleton1@gmail.com>
2015-04-06 06:36:11 -0400

    added function version of answer

--- JavaScript/prob1.js
+++ JavaScript/prob1.js
@@ -1,8 +1,19 @@
 // find the sum of all multiples of 3 or 5 below 1000
 
-var num_arr = []
+// simple for loop version
 var totalled = 0
 for (var num = 0; num < 1000; num += 1) {
     if (num % 3 == 0 || num % 5 == 0)
         totalled += num;
 };
+
+// function version
+var primed = function(max_num) {
+    var totalled = 0
+    for (var num = 0; num < max_num; num += 1)
+        if (num % 3 == 0 || num % 5 == 0)
+            totalled += num;
+    return totalled;
+};
+
+console.log(primed(1000));
